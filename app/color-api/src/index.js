b4f180ac08570c234f2bdb6ac0834558bf29a35f
git <gula16@itu.edu.tr>
2026-01-01 00:59:57 +0300

    Implementing health probes

--- app/color-api/src/index.js
+++ app/color-api/src/index.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 80;
 const color = "blue";
 const hostname = os.hostname();
 
+const delay_startup = process.env.DELAY_STARTUP === 'true';
+const fail_liveness = process.env.FAIL_LIVENESS === 'true';
+const fail_readiness = process.env.FAIL_READINESS === 'true' ? Math.random() < 0.5 : false;
+
+console.log(`delay_startup: ${delay_startup}`);
+console.log(`fail_liveness: ${fail_liveness}`);
+console.log(`fail_readiness: ${fail_readiness}`);
+
 app.get('/', (req, res) => {
     const timestamp = new Date().toISOString();
     res.send(`<h1 style="color: ${color};">. Hello from: ${appName}. Current time is: ${timestamp} </h1><h2>Hostname: ${hostname}</h2>`);
@@ -26,6 +34,36 @@ app.get('/api', (req, res) => {
     }
 });
 
+// Startup Probe
+app.get('/up', (req, res) => {
+    return res.status(200).send('OK');
+});
+
+// Liveness Probe
+app.get('/health', (req, res) => {
+     if (fail_liveness) {
+        return res.status(503).send('Not Alive');
+    }
+    return res.status(200).send('Alive');
+});
+
+// Readiness Probe
+app.get('/ready', (req, res) => {
+    if (fail_readiness) {
+        return res.status(503).send('Not Ready');
+    }
+    return res.status(200).send('Ready');
+});
+
+if (delay_startup) {
+    console.log('Delaying startup by 60 seconds...');
+    const start = Date.now();
+
+    while (Date.now() - start < 60000) {
+        // Busy-wait to simulate startup delay
+    }
+}
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+    console.log(`Server listening on port ${port}${delay_startup ? ' — startup delay 60s applied' : ''}`);
 });
